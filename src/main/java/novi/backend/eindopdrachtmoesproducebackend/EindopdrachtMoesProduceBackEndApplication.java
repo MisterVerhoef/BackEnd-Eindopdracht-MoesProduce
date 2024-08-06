0a43523a1b78633d6894e7941dc7a094fbaa14a5
MisterVerhoef <frankverhoef@outlook.com>
2024-08-06 16:32:15 +0200

    test

---
+++ src/main/java/novi/backend/eindopdrachtmoesproducebackend/EindopdrachtMoesProduceBackEndApplication.java
@@ -0,0 +1,13 @@
+package novi.backend.eindopdrachtmoesproducebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtMoesProduceBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtMoesProduceBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/novi/backend/eindopdrachtmoesproducebackend/EindopdrachtMoesProduceBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package novi.backend.eindopdrachtmoesproducebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtMoesProduceBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
