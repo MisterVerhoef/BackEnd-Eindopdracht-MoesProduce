464dad1b2704b0bd987a58cded4696fdc2dc2dc3
MisterVerhoef <frankverhoef@outlook.com>
2024-08-06 16:52:41 +0200

    test 4

---
+++ src/main/java/novi/backend/eindopdrachtmoesproducebackend/controller/UserController.java
@@ -0,0 +1,7 @@
+package novi.backend.eindopdrachtmoesproducebackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
