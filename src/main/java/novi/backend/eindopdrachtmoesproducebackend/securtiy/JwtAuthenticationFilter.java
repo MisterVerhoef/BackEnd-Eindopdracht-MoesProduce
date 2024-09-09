efefec1bfd05791d4db340c1fb56976159533d37
MisterVerhoef <frankverhoef@outlook.com>
2024-09-09 18:17:42 +0200

    random key gen werkend gekregen

--- src/main/java/novi/backend/eindopdrachtmoesproducebackend/securtiy/JwtAuthenticationFilter.java
+++ src/main/java/novi/backend/eindopdrachtmoesproducebackend/securtiy/JwtAuthenticationFilter.java
@@ -1,12 +1,11 @@
 package novi.backend.eindopdrachtmoesproducebackend.securtiy;
 
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,60 +14,54 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
-    protected void doFilterInternal( HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, java.io.IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        // Haal de "Authorization" header op uit het verzoek
         final String requestTokenHeader = request.getHeader("Authorization");
 
         String username = null;
         String jwtToken = null;
 
-        // Controleer of de header niet null is en begint met "Bearer "
         if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
-            // Haal het JWT-token op door de "Bearer " prefix te verwijderen
             jwtToken = requestTokenHeader.substring(7);
             try {
-                // Extraheer de gebruikersnaam uit het JWT-token
                 username = jwtUtil.extractUsername(jwtToken);
             } catch (ExpiredJwtException e) {
-                System.out.println("JWT Token is verlopen");
+                logger.warn("JWT Token is expired");
             } catch (IllegalArgumentException e) {
-                System.out.println("Kan JWT Token niet verkrijgen");
+                logger.warn("Unable to get JWT Token");
             }
         } else {
-            logger.warn("JWT Token begint niet met een Bearer String");
+            logger.warn("JWT Token does not begin with Bearer String");
         }
-        
-        // Controleer of de gebruikersnaam niet null is en er geen authenticatie is ingesteld in de SecurityContext
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-            // Laad de gebruikersdetails op basis van de gebruikersnaam
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
-            // Valideer het JWT-token
             if (jwtUtil.validateToken(jwtToken, userDetails)) {
-
-                // Maak een UsernamePasswordAuthenticationToken aan met de gebruikersdetails en autoriteiten
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                // Stel de authenticatie in de SecurityContext
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
-        // Ga verder met de filterketen
         filterChain.doFilter(request, response);
     }
 }
